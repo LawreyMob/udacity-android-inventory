965951b40ff43613a423773640d45a5f830847c4
Lawrence Tan <lawrence.tan@2359media.com>
2017-01-17 17:41:52 +0800

    [add] sale button [add] order button [add] edit function

---
+++ InventoryApp/app/src/main/java/com/lawrey/inventoryapp/InventoryCursorAdapter.java
@@ -0,0 +1,130 @@
+package com.lawrey.inventoryapp;
+
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.lawrey.inventoryapp.data.InventoryContract;
+
+import static android.R.attr.id;
+
+/**
+ * Created by Lawrey on 4/1/17.
+ */
+
+public class InventoryCursorAdapter extends CursorAdapter {
+
+    /**
+     * Constructs a new {@link InventoryCursorAdapter}.
+     *
+     * @param context The context
+     * @param c       The cursor from which to get the data.
+     */
+    public InventoryCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0 /* flags */);
+    }
+
+    /**
+     * Makes a new blank list item view. No data is set (or bound) to the views yet.
+     *
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already
+     *                moved to the correct position.
+     * @param parent  The parent to which the new view is attached to
+     * @return the newly created list item view.
+     */
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        // Inflate a list item view using the layout specified in list_item.xml
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+    }
+
+    /**
+     * This method binds the pet data (in the current row pointed to by cursor) to the given
+     * list item layout. For example, the name for the current pet can be set on the name TextView
+     * in the list item layout.
+     *
+     * @param view    Existing view, returned earlier by newView() method
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already moved to the
+     *                correct row.
+     */
+    @Override
+    public void bindView(View view, final Context context, Cursor cursor) {
+        // Find individual views that we want to modify in the list item layout
+        TextView nameTextView = (TextView) view.findViewById(R.id.name);
+        TextView quantityTextView = (TextView) view.findViewById(R.id.quantity);
+        TextView priceTextView = (TextView) view.findViewById(R.id.price);
+
+        // Find the columns of pet attributes that we're interested in
+        int nameColumnIndex = cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_INVENTORY_NAME);
+        int quantityColumnIndex = cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_INVENTORY_QUANTITY);
+        int priceColumnIndex = cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_INVENTORY_PRICE);
+
+
+        // Read the pet attributes from the Cursor for the current inventory
+        final String inventoryItemName = cursor.getString(nameColumnIndex);
+        int inventoryItemQuantity = cursor.getInt(quantityColumnIndex);
+        double inventoryItemPrice = cursor.getDouble(priceColumnIndex);
+
+
+        // Update the TextViews with the attributes for the current inventory
+        nameTextView.setText(inventoryItemName);
+        quantityTextView.setText("Remaining Stock : " + inventoryItemQuantity);
+        priceTextView.setText("Price : $" + String.format("%.2f", inventoryItemPrice));
+
+        Button onOrderMoreButton = (Button) view.findViewById(R.id.button_order_more);
+
+        onOrderMoreButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("message/rfc822");
+                i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"orders@udacitytesting.com"});
+                i.putExtra(Intent.EXTRA_SUBJECT, "Order more " + inventoryItemName);
+                i.putExtra(Intent.EXTRA_TEXT   , "Put in your requirements here");
+                try {
+                    context.startActivity(Intent.createChooser(i, "Send mail..."));
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(context, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        int inventoryIDColumnIndex = cursor.getColumnIndex(InventoryContract.InventoryEntry._ID);
+        int inventoryID = cursor.getInt(inventoryIDColumnIndex);
+
+        Button onSaleButton = (Button) view.findViewById(R.id.button_sale);
+
+        final Uri currentInventoryUri = ContentUris.withAppendedId(InventoryContract.InventoryEntry.CONTENT_URI, inventoryID);
+
+        final ContentValues values = new ContentValues();
+        values.put(InventoryContract.InventoryEntry.COLUMN_INVENTORY_NAME, inventoryItemName);
+        values.put(InventoryContract.InventoryEntry.COLUMN_INVENTORY_PRICE, inventoryItemPrice);
+
+        final int adjustedItemQuantity = inventoryItemQuantity - 1;
+
+        onSaleButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                if (adjustedItemQuantity >= 0) {
+                    values.put(InventoryContract.InventoryEntry.COLUMN_INVENTORY_QUANTITY, adjustedItemQuantity);
+                    int rowsAffected = context.getContentResolver().update(currentInventoryUri, values, null, null);
+                    Log.v("rowsAffected", "" + rowsAffected);
+                }else{
+                    Toast.makeText(context, "No more stock for sale.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
---
+++ InventoryApp/app/src/main/java/com/lawrey/inventoryapp/data/InventoryContract.java
@@ -0,0 +1,98 @@
+package com.lawrey.inventoryapp.data;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+import static android.text.style.TtsSpan.GENDER_FEMALE;
+import static android.text.style.TtsSpan.GENDER_MALE;
+
+/**
+ * Created by Lawrey on 4/1/17.
+ */
+
+public class InventoryContract {
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    private InventoryContract() {}
+
+    /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = "com.lawrey.inventoryapp";
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.lawrey.inventoryapp/inventory/ is a valid path for
+     * looking at inventory data. content://com.example.android.inventory/staff/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "staff".
+     */
+    public static final String PATH_INVENTORY = "inventory";
+
+    /**
+     * Inner class that defines constant values for the pets database table.
+     * Each entry in the table represents a single pet.
+     */
+    public static final class InventoryEntry implements BaseColumns {
+
+        /** The content URI to access the inventory data in the provider */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_INVENTORY);
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of inventory.
+         */
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_INVENTORY;
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single inventory item.
+         */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_INVENTORY;
+
+        /** Name of database table for inventory */
+        public final static String TABLE_NAME = "inventory";
+
+        /**
+         * Unique ID number for the inventory item (only for use in the database table).
+         *
+         * Type: INTEGER
+         */
+        public final static String _ID = BaseColumns._ID;
+
+        /**
+         * Name of the inventory.
+         *
+         * Type: TEXT
+         */
+        public final static String COLUMN_INVENTORY_NAME ="name";
+
+        /**
+         * Quantity of the inventory item.
+         *
+         * Type: INTEGER
+         */
+        public final static String COLUMN_INVENTORY_QUANTITY = "quantity";
+
+        /**
+         * Price of the inventory item.
+         *
+         * Can accept decimals but most be more than 0
+         *
+         * Type: REAL
+         */
+        public final static String COLUMN_INVENTORY_PRICE = "price";
+
+    }
+
+}
